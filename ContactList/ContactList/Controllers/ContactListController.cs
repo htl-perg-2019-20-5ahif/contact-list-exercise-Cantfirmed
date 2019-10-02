0b15a1ba45423db5fd3e006e39b9e4de7707787f
Patrick Wegerer <patrick.wegerer@gmail.com>
2019-10-02 13:58:21 +0200

    Updated solution

---
+++ ContactList/ContactList/Controllers/ContactListController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace ContactList.Controllers {
+    [ApiController]
+    [Route("api/contacts")]
+    public class CLController : ControllerBase {
+        private static List<Person> contactList = new List<Person>();
+        [HttpGet]
+        public IActionResult GetAllContacts() {
+            return Ok(contactList);
+        }
+
+        [HttpPost]
+        public IActionResult AddContact([FromBody] Person newPerson) {
+            contactList.Add(newPerson);
+            return Created("", newPerson);
+        }
+
+        [HttpPut]
+        [Route("{index}")]
+        public IActionResult UpdateContact(int index, [FromBody] Person newPerson) {
+            if (index >= 0 && index < contactList.Count) {
+                contactList[index] = newPerson;
+                return Ok();
+            }
+            return BadRequest("Invalid Index");
+        }
+
+        [HttpDelete]
+        [Route("{personId}")]
+        public IActionResult DeleteContact(int personId) {
+            foreach (var contact in contactList) {
+                if (contact.id.Equals(personId)) {
+                    contactList.Remove(contact);
+                    return NoContent();
+                }
+            }
+            return BadRequest("Invalid Index");
+        }
+
+        [HttpGet]
+        [Route("findByName")]
+        public IActionResult findByName([FromQuery]String name) {
+            if (string.IsNullOrEmpty(name)) {
+                return BadRequest("Invalid or missing name");
+            }
+            return Ok(contactList.Where(person => person.firstName.Equals(name) || person.lastName.Equals(name)));
+        }
+    }
+}
---
+++ ContactList/ContactList/Person.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ContactList {
+    public class Person {
+        public int id { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+    }
+}
